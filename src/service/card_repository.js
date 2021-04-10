edd376c313849af143555d3b03b363c91d8145bc
OH DONG WOONG <integral5137@gmail.com>
2021-04-10 22:05:08 +0900

    implemation user data realtime read & update in  card_repository component

--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -1,12 +1,27 @@
 import firebaseApp from "./firebase";
 
 class CardRepository {
+  readCard(userId, updateUserCard) {
+    const userDataRef = firebaseApp.database().ref(`${userId}/cards`);
+    userDataRef.on("value", (snapshot) => {
+      const data = snapshot.val();
+      data && updateUserCard(data);
+    });
+    return () => userDataRef.off();
+  }
+
   saveCard(userId, card) {
-    firebaseApp.database().ref(`${userId}/cards/${card.id}`).set(card);
+    firebaseApp //
+      .database()
+      .ref(`${userId}/cards/${card.id}`)
+      .set(card);
   }
 
   removeCard(userId, card) {
-    firebaseApp.database().ref(`${userId}/cards/${card.id}`).remove();
+    firebaseApp //
+      .database()
+      .ref(`${userId}/cards/${card.id}`)
+      .remove();
   }
 }
 
